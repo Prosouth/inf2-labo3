430585c1efe1b5bd673a3b53aba0904f5a0c9c7c
Sebastien Saez <monsieursaez@gmail.com>
2017-03-20 16:49:45 +0100

    changed header

--- Date.h
+++ Date.h
@@ -1,10 +1,10 @@
 /* -------------------------------------------------------------------------------
  Laboratoire: 3 - Fonction générique
- Fichier    : Point.h
+ Fichier    : Date.h
  Auteur(s)  : Kevin Cristi et Sébastien Saez
  Date       : 20 mars 2017
  
-But        : Ce fichier met à disposition la classe Date avec les attributs jour,
+But         : Ce fichier met à disposition la classe Date avec les attributs jour,
  *            mois et année. Afin de pouvoir comparer une date, nous avons surchargé
  *            deux opérateurs: l'opérateur > afin de pouvoir faire des comparaisons 
  *            entre deux points et l'opérateur << pour afficher.
