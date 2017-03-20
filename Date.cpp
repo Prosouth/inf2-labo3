da33c35d57758aef7f3ebb98e7b171c1c02ad813
Sebastien Saez <monsieursaez@gmail.com>
2017-03-20 13:18:07 +0100

    everything working

--- Date.cpp
+++ Date.cpp
@@ -18,8 +18,23 @@ Date::Date(short jour, short mois, short annee)
 
 bool operator > (const Date& date1, const Date& date2)
 {
-    return (date2.jour > date1.jour && date2.mois > date1.mois &&
-            date2.annee > date1.annee);
+    // année => année mois => année jour
+    if(date1.annee > date2.annee)
+    {
+        return true;
+    }
+    else if(date1.mois > date2.mois)
+    {
+        return true;
+    }
+    else if(date1.jour > date2.jour)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 std::ostream& operator << (std::ostream& os, const std::vector<Date>& vectDate)
